bfef58ace5c22e7708993be53c1b7ea49bee9620
Tim Ruther <ruther.tim1@gmail.com>
2026-06-02 01:12:52 +0200

    V3

---
+++ WM_Prediction.py
@@ -0,0 +1,11 @@
+import numpy as np
+
+import WM_structure
+from WM_structure import Gruppen
+
+print(Gruppen)
+
+
+
+
+
---
+++ WM_structure.py
@@ -0,0 +1,38 @@
+import numpy as np
+#Gruppen Dictionary:
+Gruppen = {
+    "Gruppe A": ["Mexiko", "Südafrika", "Republik Korea", "Tschechien"],
+    "Gruppe B": ["Kanada", "Bosnien und Herzegowina", "Katar", "Schweiz"],
+    "Gruppe C": ["Brasilien", "Marokko", "Haiti", "Schottland"],
+    "Gruppe D": ["USA", "Paraguay", "Australien", "Türkei"],
+    "Gruppe E": ["Deutschland", "Curaçao", "Elfenbeinküste", "Ecuador"],
+    "Gruppe F": ["Niederlande", "Japan", "Schweden", "Tunesien"],
+    "Gruppe G": ["Belgien", "Ägypten", "IR Iran", "Neuseeland"],
+    "Gruppe H": ["Spanien", "Kap Verde", "Saudi-Arabien", "Uruguay"],
+    "Gruppe I": ["Frankreich", "Senegal", "Irak", "Norwegen"],
+    "Gruppe J": ["Argentinien", "Algerien", "Österreich", "Jordanien"],
+    "Gruppe K": ["Portugal", "DR Kongo", "Usbekistan", "Kolumbien"],
+    "Gruppe L": ["England", "Kroatien", "Ghana", "Panama"],
+}
+
+# sechtzentelfinale
+Sechzehntel= {
+
+}
+#Achtelfinale
+Achtelfinale = {
+    "Spiel A": [" A1","B2"],
+    "Spiel B": [],
+    "Spiel C": [],
+    "Spiel D": [], 
+    "Spiel E": [],
+    "Spiel F": [],
+    "Spiel G": [],
+    "Spiel H": [],
+}
+
+#1Viertelfinale
+
+#1 Halbfinale
+
+#Finale
