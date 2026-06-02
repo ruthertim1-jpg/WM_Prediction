198fa8b1c6a7814fb8502c3deb3871a8886bbbef
Tim Ruther <ruther.tim1@gmail.com>
2026-06-02 02:03:14 +0200

    V4

--- WM_Prediction.py
+++ WM_Prediction.py
@@ -1,11 +1,29 @@
 import numpy as np
+from itertools import combinations
 
 import WM_structure
 from WM_structure import Gruppen
 
-print(Gruppen)
+print(Gruppen["Gruppe A"])
+
+# wer speilt gegen wen
+gesamt_spielplan = []
+
+for Gruppe , Teams in Gruppen.items():
+    paarungen = list(combinations(Teams,2))
+
+    spiele_pro_gruppe = []
+    
+    
+    for team1, team2 in paarungen:
+        spiele_pro_gruppe.append(f"{team1} gegen {team2}")
+        #print(f"{team1} gegen {team2}")
+    
+    gruppen_spielplan = {
+        "Gruppe": Gruppe,
+        "Spiele": spiele_pro_gruppe,
+        }
+    gesamt_spielplan.append(gruppen_spielplan)
 
 
-
-
-
+print(gesamt_spielplan[0])
