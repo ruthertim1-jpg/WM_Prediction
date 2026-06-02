643782e8701818d22a5687d6e65e8109fa239b23
Tim Ruther <ruther.tim1@gmail.com>
2026-06-02 12:33:45 +0200

    Update WM_Prediction.py

--- WM_Prediction.py
+++ WM_Prediction.py
@@ -4,26 +4,28 @@ from itertools import combinations
 import WM_structure
 from WM_structure import Gruppen
 
-print(Gruppen["Gruppe A"])
+
+
 
 # wer speilt gegen wen
 gesamt_spielplan = []
 
 for Gruppe , Teams in Gruppen.items():
-    paarungen = list(combinations(Teams,2))
+    paarungen = list(combinations(Teams,2)) #´combinations# verhindet doppelte paarungen
 
-    spiele_pro_gruppe = []
+    spiele_pro_gruppe = [] 
     
     
     for team1, team2 in paarungen:
-        spiele_pro_gruppe.append(f"{team1} gegen {team2}")
+        spiele_pro_gruppe.append(f"{team1} gegen {team2}") # append fügt zu bereits bestehenden liste hinten das hinzu, wass in seiner Klammer kommt
         #print(f"{team1} gegen {team2}")
     
     gruppen_spielplan = {
         "Gruppe": Gruppe,
         "Spiele": spiele_pro_gruppe,
-        }
+         "":print()
+    }
     gesamt_spielplan.append(gruppen_spielplan)
 
 
-print(gesamt_spielplan[0])
+print(gesamt_spielplan)
